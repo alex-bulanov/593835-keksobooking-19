720d4bf678eb8d91d184a62153348b30ab8c53a3
AlexBulanov <alex-bulanov.mail@yandex.ru>
2020-02-09 17:34:04 +0300

    перемещение пина и отображение адреса

--- js/data.js
+++ js/data.js
@@ -5,7 +5,10 @@
   var MAX_TITLE_LENGTH = 100;
   var MAX_PRICE_VALUE = 1000000;
   var PINS_QUANTITY = 8;
-
+  var MAX_PIN_X_OFFSET = 1168;
+  var MIN_PIN_X_OFFSET = -32;
+  var MAX_PIN_Y_OFFSET = 623;
+  var MIN_PIN_Y_OFFSET = 2;
   var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
   var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
   var checkTimes = ['12:00', '13:00', '14:00'];
@@ -31,6 +34,10 @@
     MAX_TITLE_LENGTH: MAX_TITLE_LENGTH,
     MAX_PRICE_VALUE: MAX_PRICE_VALUE,
     PINS_QUANTITY: PINS_QUANTITY,
+    MAX_PIN_X_OFFSET: MAX_PIN_X_OFFSET,
+    MIN_PIN_X_OFFSET: MIN_PIN_X_OFFSET,
+    MAX_PIN_Y_OFFSET: MAX_PIN_Y_OFFSET,
+    MIN_PIN_Y_OFFSET: MIN_PIN_Y_OFFSET,
     apartmentPriceByKey: apartmentPriceByKey,
     apartmentNamesByKey: apartmentNamesByKey,
     features: features,
--- js/map.js
+++ js/map.js
@@ -40,6 +40,59 @@
     renderCard(currentCard);
   };
 
+  var onMainPinMouseDown = function (evtDown) {
+    evtDown.preventDefault();
+
+    var startCoords = {
+      x: evtDown.clientX,
+      y: evtDown.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      if ((pinMain.offsetLeft - shift.x) <= window.data.MAX_PIN_X_OFFSET && (pinMain.offsetLeft - shift.x) >= window.data.MIN_PIN_X_OFFSET) {
+        pinMain.style.left = (pinMain.offsetLeft - shift.x) + 'px';
+      }
+
+      if ((pinMain.offsetTop - shift.y) <= window.data.MAX_PIN_Y_OFFSET && (pinMain.offsetTop - shift.y) >= window.data.MIN_PIN_Y_OFFSET) {
+        pinMain.style.top = (pinMain.offsetTop - shift.y) + 'px';
+      }
+
+      window.form.checkAndSetInputAddress();
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          pinMain.removeEventListener('click', onClickPreventDefault);
+        };
+        pinMain.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  };
 
   var onMainPinLeftMouseClick = function (evt) {
     var map = document.querySelector('.map');
@@ -61,6 +114,7 @@
   };
 
   window.form.setDisabledFormCondition();
+  pinMain.addEventListener('mousedown', onMainPinMouseDown);
   pinMain.addEventListener('click', onMainPinLeftMouseClick);
 
 })();
